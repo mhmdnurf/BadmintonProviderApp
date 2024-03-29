2af3aba2bae883cb8757c719716897b61d74378c
mhmdnurf <mhmd.nurf@gmail.com>
2024-03-30 00:56:42 +0700

    fetching data for detail pemesan is running good

--- src/components/detail_pemesan/PemesanField.tsx
+++ src/components/detail_pemesan/PemesanField.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 import {Pressable, StyleSheet, Text, View} from 'react-native';
 import InputField from '../InputField';
 
-const PemesanField = () => {
+interface DataBooking {
+  lamaBermain: string;
+}
+
+interface DataUser {
+  namaLengkap: string;
+  nomor: string;
+}
+
+interface PemesananField {
+  dataBooking: DataBooking;
+  dataUser: DataUser;
+}
+
+const PemesanField = ({dataBooking, dataUser}: PemesananField) => {
   return (
     <>
       <View style={styles.container}>
         <Text style={styles.label}>Nama Lengkap</Text>
-        <InputField placeholder="Nama Lengkap" />
+        <InputField placeholder="Nama Lengkap" value={dataUser.namaLengkap} />
         <Text style={styles.label}>Nomor Telepon</Text>
-        <InputField placeholder="Nomor Telepon" />
+        <InputField placeholder="Nomor Telepon" value={dataUser.nomor} />
         <Text style={styles.label}>Durasi</Text>
-        <InputField placeholder="Durasi" />
+        <InputField placeholder="Durasi" value={dataBooking.lamaBermain} />
         <Pressable style={styles.btnHapus} onPress={() => console.log('ok')}>
           <Text style={styles.btnText}>Hapus</Text>
         </Pressable>
--- src/screens/DetailPemesan.tsx
+++ src/screens/DetailPemesan.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../components/Header';
 import RootContainer from '../components/RootContainer';
 import PemesanField from '../components/detail_pemesan/PemesanField';
+import firestore from '@react-native-firebase/firestore';
+import {ActivityIndicator} from 'react-native';
 
 interface DetailPemesan {
   route: any;
@@ -9,13 +11,56 @@ interface DetailPemesan {
 
 const DetailPemesan = ({route}: DetailPemesan) => {
   const {booking_uid} = route.params;
-  console.log(booking_uid);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [dataBooking, setDataBooking] = React.useState<any>({});
+  const [dataUser, setDataUser] = React.useState<any>({});
+  const fetchBooked = React.useCallback(async () => {
+    try {
+      const query = await firestore()
+        .collection('booking')
+        .doc(booking_uid)
+        .get();
+      setDataBooking(query.data());
+    } catch (error) {
+      console.log('Error fetching data: ', error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [booking_uid]);
+
+  const fetchUser = React.useCallback(async () => {
+    try {
+      const query = await firestore()
+        .collection('users')
+        .doc(dataBooking.user_uid)
+        .get();
+      setDataUser(query.data());
+    } catch (error) {
+      console.log('Error fetching data: ', error);
+    }
+  }, [dataBooking.user_uid]);
+
+  React.useEffect(() => {
+    fetchBooked();
+  }, [fetchBooked]);
+
+  React.useEffect(() => {
+    if (dataBooking.user_uid) {
+      fetchUser();
+    }
+  }, [dataBooking.user_uid, fetchUser]);
   return (
     <>
       <RootContainer backgroundColor="white">
         <Header title="Detail Pemesan" />
-        <Header title="Lapangan 5" />
-        <PemesanField />
+        {isLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <>
+            <Header title={`Lapangan ${dataBooking.lapangan}`} />
+            <PemesanField dataBooking={dataBooking} dataUser={dataUser} />
+          </>
+        )}
       </RootContainer>
     </>
   );
